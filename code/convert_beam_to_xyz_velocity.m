4d975faac20b02bf0975c7612705f9557511a3aa
Hayden Johnson <h.johnson1701@gmail.com>
2026-09-01 15:10:38 -0800

    Added thorough comments to convert_beam_to_xyz_velocity.m

--- code/convert_beam_to_xyz_velocity.m
+++ code/convert_beam_to_xyz_velocity.m
@@ -1,7 +1,24 @@
 function [vel_xyz] = convert_beam_to_xyz_velocity(beam1,beam2,beam3,beam4,beamAngle,excludeBeam)
     
-    % matrix to convert from beam velocities to xyz velocity in instrument
-    % coordinate frame
+    % Function to convert along-beam velocities to vector velocity in
+    % instrument frame coordinates. Note that the beam velocities are
+    % assumed to specified at consistent depths - i.e. instrument tilt
+    % needs to be corrected for before calling this function.
+
+    % Inputs:
+    % beamN: (m/s) [1xn] Velocity measured by beam N. Positive is away from 
+    % transducer. n is the number of depth bins.
+    % beamAngle: (degrees) [scalar] Angle of the beams from the z axis in
+    % the coordinate frame (25 degrees for Signature instruments).
+    % excludeBeam: [int] Beam number to exclude for a 3-beam solution. If
+    % 0, then a 4-beam solution is calculated.
+
+    % Outputs:
+    % vel_xyz: (m/s) [3xn] Vector velocity in the instrument coordinate
+    % frame at each of the n depth bins of the input beam velocities.
+
+    % Hayden Johnson, 2026-09-01
+
     if excludeBeam == 0
         conversion_matrix = [1/(2*sind(beamAngle)) 0 -1/(2*sind(beamAngle)) 0;
             0 -1/(2*sind(beamAngle)) 0 1/(2*sind(beamAngle));
